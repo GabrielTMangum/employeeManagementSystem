5716c806cbbf7881706aaeef3e272aaf2c1fa134
GabrielTMangum <gabrieltmangum@gmail.com>
2019-11-24 14:33:47 -0500

    initial push

---
+++ server.js
@@ -0,0 +1,13 @@
+const mySQL = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "rootroot",
+    database: "employeeManagementSystem_Db"
+  });
+
